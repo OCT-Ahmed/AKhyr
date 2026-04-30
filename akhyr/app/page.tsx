ffd77c35d2f4269b9718a8364e44a43259da87cc
Ahmed-Satoru <ahmedweb2007@gmail.com>
2026-04-30 06:18:22 +0000

    feat: create footer with links, brand namde and social media icons.

--- akhyr/app/page.tsx
+++ akhyr/app/page.tsx
@@ -1,15 +1,26 @@
+import Link from "next/link";
+
 const HomePage = () => {
   return (
     <div className="flex items-stretch justify-between h-[calc(100vh-72px)]  gap-4 p-16">
       
       {/* LEFT */}
       <div className="w-full flex flex-col justify-center">
-        <h1 className="font-inter text-5xl font-semibold mb-6">Imagine being a part of <span className="text-4xl block">someone success story</span></h1>
+        <h1 className="font-sans text-5xl font-semibold mb-6">Imagine being a part of <span className="text-4xl block">someone success story</span></h1>
         <p className="font-inter text-muted leading-[1.6em]">I'm trying to build my future from zero. I'm Ahmed.. a Sudanese student who want to chase his dreams. And have a chance to study in a college</p>
         {/* CTA BUTTONS */}
         <div className="flex gap-4 mt-6">
-          <button className="text-white bg-primary px-8 py-3 rounded-xl hover:bg-primary/75 transition-all duration-300 cursor-pointer" id="cta-button">Join Our Story</button>
-          <button className="text-white bg-white/5 filter-blur-xl border-2 border-white/25 px-8 py-3 rounded-xl hover:bg-white/0 transition-all duration-300 cursor-pointer" id="cta-button">Read My Story</button>
+          <Link href="/subscription">
+            <button className="text-white bg-primary px-8 py-3 rounded-xl hover:bg-primary/75 transition-all duration-300 cursor-pointer" id="cta-button">
+              Join Our Story
+            </button>
+          </Link>
+          <Link href="/story">
+            <button className="text-white bg-white/5 filter-blur-xl border-2 border-white/25 px-8 py-3 rounded-xl hover:bg-white/0 transition-all duration-300 cursor-pointer" id="cta-button">
+              Read My Story
+            </button>
+          </Link>
+          
         </div>
       </div>
       {/* RIGHT */}
